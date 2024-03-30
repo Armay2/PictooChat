4cc7d22fcd02a6f70a8cca4339a3eeae1fbfb887
Arnaud <arnaud.nommay@orange.fr>
2024-03-30 23:07:04 +0100

    wip: Picture interaction view

---
+++ New Apps Test/Scenes/Chat/PictooView.swift
@@ -0,0 +1,18 @@
+//
+//  PictooView.swift
+//  New Apps Test
+//
+//  Created by Arnaud NOMMAY on 31/03/2024.
+//
+
+import SwiftUI
+
+struct PictooView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PictooView()
+}
