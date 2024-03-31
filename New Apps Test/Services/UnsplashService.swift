63ac050ffb5b35f842a16293c4212f8cc3bc86be
Arnaud <arnaud.nommay@orange.fr>
2024-03-31 22:27:46 +0200

    doc: add documentation and refacto

--- New Apps Test/Services/UnsplashService.swift
+++ New Apps Test/Services/UnsplashService.swift
@@ -7,15 +7,24 @@
 
 import Foundation
 
-class UnsplashService {
+/// A service for fetching photos from the Unsplash API.
+struct UnsplashService {
+    /// The networker responsible for executing network requests.
     private let networker = Networker()
 
+    /// The application ID provided by Unsplash when you register your application.
     private let appId = "575727"
+    
+    /// The access key provided by Unsplash, used to authenticate API requests.
     private let accessKey = "8j7cdQS9HM1tfIomk_dUwqrIZ7wnymEECz6xk6OPP6k"
+    
+    /// The base URL for the Unsplash API.
     private let baseURL = "https://api.unsplash.com/"
     
+    /// Fetches an array of featured photo URLs from Unsplash.
+    /// - Returns: An array of strings, each representing the URL of a regular-sized photo from Unsplash. Returns an empty array if the request fails or if decoding fails.
     func getFeaturedPhotos() async -> [String] {
-        guard let url =  URL(string: baseURL + "/photos/?client_id=" + accessKey) else {
+        guard let url = URL(string: baseURL + "/photos/?client_id=" + accessKey) else {
             return []
         }
         
@@ -30,10 +39,14 @@ class UnsplashService {
     }
 }
 
+/// A structure representing the response for a photo fetched from Unsplash.
 struct ImageInfo: Codable {
+    /// A structure containing various URL formats of a photo.
     let urls: ImageUrl
 }
 
+/// A structure representing different sizes of URLs for a single photo from Unsplash.
 struct ImageUrl: Codable {
     let regular: String
 }
+
